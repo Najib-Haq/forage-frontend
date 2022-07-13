6a782dd562f1278256bca3139e91aa0f0a10cf08
Najibul Haque Sarker <najibhaq98@gmail.com>
2022-07-14 04:09:35 +0600

    sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,19 +3,57 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import logo from '../forage_logo.png'
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
-const drawerWidth = 240;
+import logo from '../forage_logo.png';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import NoteAltOutlinedIcon from '@mui/icons-material/NoteAltOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import ContentPasteSearchOutlinedIcon from '@mui/icons-material/ContentPasteSearchOutlined';
+
+import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
+import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import ScheduleSendOutlinedIcon from '@mui/icons-material/ScheduleSendOutlined';
+
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+
+import '../styles/Sidebar.css';
+
+const drawerWidth = 250;
+const selectStyle = {
+  textAlign : 'left',
+  maxHeight : '80%',
+  color: '#C3C3C3', // TODO: change this with default value stuff
+}
+const footerMenuStyle = { display: 'flex', flexDirection: 'row', padding: 0, paddingLeft: 7, alignItems: 'center', justifyContent: 'center'}
+
+const quickAccessLabels = [
+  'Home', 'My Tasks', 'Uncategorized', 'Reviews'
+]
+const quickAccessLogos = [
+  <HomeOutlinedIcon/>, <NoteAltOutlinedIcon/>, <VisibilityOutlinedIcon/>, <ContentPasteSearchOutlinedIcon/>,
+]
+const projectAccessLabels = [
+  'Papers', 'Tasks', 'Schedule', 'Submission'
+]
+const projectAccessLogos = [
+  <FeedOutlinedIcon/>, <ListAltOutlinedIcon/>, <CalendarMonthOutlinedIcon/>, <ScheduleSendOutlinedIcon/>,
+]
+const footerLogos = [
+  <PermIdentityOutlinedIcon/>, <SettingsOutlinedIcon/>, <NotificationsNoneOutlinedIcon/>,
+]
+
 
 export default function PermanentDrawerLeft() {
   return (
@@ -26,6 +64,7 @@ export default function PermanentDrawerLeft() {
         sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
       >
       </AppBar>
+
       <Drawer
         sx={{
           width: drawerWidth,
@@ -33,74 +72,83 @@ export default function PermanentDrawerLeft() {
           '& .MuiDrawer-paper': {
             width: drawerWidth,
             boxSizing: 'border-box',
+            borderWidth: 1,
+            borderColor: '#C3C3C3',
           },
         }}
         variant="permanent"
         anchor="left"
       >
-        <Toolbar 
+
+        <ListItemIcon>
+          {<img src={logo} className="App-logo" alt="logo" />}
+        </ListItemIcon>
+        {/* <Toolbar 
             children={<img src={logo} className="App-logo" alt="logo" />}
-        />
-        <Divider />
+        /> */}
+
+        <Divider variant='middle' light={true}/>
+
+        <label className='Sidebar-header'> Quick Access </label>
+
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {quickAccessLabels.map((text, index) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
+              <ListItemButton selected={index===2} sx={index===2 ? {borderRadius: '5%', ml: 1, mr: 1, backgroundColor: '#D9D9D9'} : {ml: 1, mr: 1,}}>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  {quickAccessLogos.at(index)}
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
-        <Divider />
+
+        <label className='Sidebar-header'> Project  </label>
+
+        <FormControl sx={{ m: 2 }}>
+            {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
+            <Select
+                value={0}
+                // onChange={handleChange}
+                displayEmpty
+                sx= {selectStyle} //{styles.Select}
+                // inputProps={{ 'aria-label': 'Without label' }}
+            >
+                <MenuItem value={0}>Select Project</MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+        </FormControl>
+
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          {projectAccessLabels.map((text, index) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
+              <ListItemButton selected={index===2} sx={index===2 ? {borderRadius: '5%', ml: 1, mr: 1, backgroundColor: '#D9D9D9'} : {ml: 1, mr: 1, }}>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  {projectAccessLogos.at(index)}
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
+        
+        <div className="bottomPush">
+          <List style={footerMenuStyle}>
+            {footerLogos.map((footerLogo, index) => (
+                <ListItem key={index} disablePadding>
+                  <ListItemButton sx={{padding: 1.4,}}>
+                    <ListItemIcon>
+                      {footerLogo}
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+          </List>
+        </div>
       </Drawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
     </Box>
   );
 }

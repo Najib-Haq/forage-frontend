1a3fc5cb050a3a6f354dd35083787019a3b8e92a
Najibul Haque Sarker <najibhaq98@gmail.com>
2022-08-10 04:52:53 +0600

    submission timeline and modal accordion

---
+++ src/components/SubmissionModal.js
@@ -0,0 +1,169 @@
+import React, {useState, useEffect} from "react";
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Divider } from "@mui/material";
+import SubmissionStep from "../components/SubmissionStep"
+
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+
+import { getStorageToken } from "../context/Auth";
+
+/* 
+    define props=> 
+        isOpen: will open the modal
+        handleClose: what to do when closing the modal
+        data: data to display 
+*/
+
+
+const URL = process.env.REACT_APP_API_URL;
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 'auto',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+  };
+
+
+const Accordion = styled((props: AccordionProps) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+    ))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    '&:not(:last-child)': {
+        borderBottom: 0,
+    },
+    '&:before': {
+        display: 'none',
+    },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+    <MuiAccordionSummary
+        expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+        {...props}
+    />
+))(({ theme }) => ({
+    backgroundColor:
+        theme.palette.mode === 'dark'
+        ? 'rgba(255, 255, 255, .05)'
+        : 'rgba(0, 0, 0, .03)',
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+        transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+        marginLeft: theme.spacing(1),
+    },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+  
+
+export default function SubmissionModal(props) {
+    const [data, setData] = useState({});
+    const [expanded, setExpanded] = React.useState('panel1');
+
+    const handleChange =
+        (panel) => (event, newExpanded) => {
+        setExpanded(newExpanded ? panel : false);
+    };
+
+    // useEffect(() => {
+    //     if(props.data.id)
+    //         fetch(URL + `api/papers/${props.data.id}`,
+    //             {
+    //                 method: 'GET',
+    //                 credentials: "same-origin",
+    //                 headers: {
+    //                         'Authorization': `Token ${getStorageToken()}`,
+    //                         'Content-Type':'application/json'
+    //                 }
+    //             })
+    //             .then(resp=>{
+    //                 if (resp.status >= 400) throw new Error();
+    //                 return resp.json();
+    //             })
+    //             .then(resp=>{
+    //                 console.log("data is :", resp)
+    //                 setData(resp)
+    //             })
+    //             .catch(error=>{
+    //                 console.log(error);
+    //             })
+    // }, [props.isOpen]);
+    
+  
+    return (
+        <Modal
+          open={props.isOpen}
+          onClose={props.handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <SubmissionStep />
+            
+                <div>
+                    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                        <Typography>Collapsible Group Item #1</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                        </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+                        <Typography>Collapsible Group Item #2</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                        </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+                        <Typography>Collapsible Group Item #3</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                        </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+            </Box>
+        </Modal>
+    );
+  }
+  
--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
+import SubmissionStep from "../components/SubmissionStep"
+import SubmissionModal from "../components/SubmissionModal"
+
+const steps = [
+    'Select master blaster campaign settings',
+    'Create an ad group',
+    'Create an ad',
+  ];
 
 export default function Submission() {
+    const [openModal, setOpenModal] = useState(true);
+
+    const handleModalClose = () => {
+        setOpenModal(false);
+    }
+
     return (
         <React.Fragment>
             <h1>Submission Page</h1>
+            <SubmissionStep/>
+
+            <SubmissionModal isOpen={openModal} handleClose={handleModalClose}/>
         </React.Fragment>
     )
 }

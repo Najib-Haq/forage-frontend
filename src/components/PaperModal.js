acf4c836322e5cef485eb29e583895c922f2ef58
Mushtari Sadia <midnight.scream98@gmail.com>
2022-08-11 12:38:59 +0600

    added tabs in note

--- src/components/PaperModal.js
+++ src/components/PaperModal.js
@@ -3,6 +3,12 @@ import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { Divider } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 import { getStorageToken } from "../context/Auth";
 
@@ -29,9 +35,49 @@ const style = {
     pb: 3,
   };
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+        >
+        {value === index && (
+            <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+            </Box>
+        )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+
 
 export default function BasicModal(props) {
     const [data, setData] = useState({});
+    const [note, setNote] = useState("");
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     useEffect(() => {
         if(props.data.id)
@@ -65,25 +111,90 @@ export default function BasicModal(props) {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
+            {/* <Box sx={style}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Item One" {...a11yProps(0)} />
+                    <Tab label="Item Two" {...a11yProps(1)} />
+                    <Tab label="Item Three" {...a11yProps(2)} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    Item One
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item Three
+                </TabPanel>
+            </Box> */}
             <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    {data.id} 
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    {data.name}
-                </Typography>
-                <Divider light />
-                {
-                    data.venue  && 
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        {data.venue.name}
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Basic" {...a11yProps(0)} />
+                        <Tab label="Notes" {...a11yProps(1)} />
+                        <Tab label="Relevant" {...a11yProps(2)} />
+                </Tabs>
+                <TabPanel value={value} index={0}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                    <b>{data.name}</b>
                     </Typography>
-                }
-                
-                <Divider light />
-                <Typography id="modal-modal-description" sx={{ mt: 1 }}>
-                    {data.abstract}
-                </Typography>
+                    {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        <b>{data.name}</b>
+                    </Typography> */}
+                    <Divider light />
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <b>Authors: </b> {data.authors}
+                    </Typography>
+                    <Divider light />
+                    <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+                    <b>Abstract: </b> 
+                        {data.abstract}
+                    </Typography>
+
+                    <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+                    <b>Note: </b> 
+                        {data.note}
+                    </Typography>
+                    
+                    <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+                    <TextField fullWidth id="outlined-basic" label="Add Note" variant="outlined" onChange={(e)=>setNote(e.target.value)}
+                value={note}/>
+                    <Stack direction="row" spacing={2}>
+                    
+                    <Button variant="outlined" href="#outlined-buttons" onClick={() => {
+        alert(note);
+        //have to change this according to backend implementation
+        fetch(URL + "api/note-to-paper/",
+                {
+                    method: 'POST',
+                    credentials: "same-origin",
+                    headers: {
+                        'Authorization': `Token ${getStorageToken()}`,
+                        'Content-Type':'application/json'
+                    },
+                    body: JSON.stringify({ 'ppid':props.data.id, 'note':note })
+                })
+                .then(resp => {
+                    if (resp.status == 200)
+                        return resp.json();
+                    else if (resp.status >= 400){
+                        if (resp.status == 500) throw new Error();
+                        return resp.json();
+                    }
+                })
+                .then(resp => {
+                    console.log(resp);
+                })
+                .catch(error=>{
+                    console.log(error);
+                })
+    }}>
+                        Submit
+                    </Button>
+                    </Stack>
+                    </Typography>
+                </TabPanel>     
             </Box>
         </Modal>
     );

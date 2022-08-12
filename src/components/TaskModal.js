1abc12ad2089043417dbb4c8555439aef3344b20
Najibul Haque Sarker <najibhaq98@gmail.com>
2022-08-13 04:08:55 +0600

    create new tasks

--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -17,43 +17,46 @@ import ListItemText from '@mui/material/ListItemText';
 
 
 import { getStorageToken } from "../context/Auth";
+import { getStorageProjID } from "../context/ProjectID";
 
 
 /* 
     define props=> 
         isOpen: will open the modal
         handleClose: what to do when closing the modal
-        data: data to display 
+        data: data to display
+        projectTask: if True then project Tasks else user tasks only
 */
 
 const URL = process.env.REACT_APP_API_URL;
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 'auto',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    pt: 2,
-    px: 4,
-    pb: 3,
-  };
-
-
 export default function TaskModal(props) {
-    const [task, setTask] = useState(null);
+    const [task, setTask] = useState({
+        id: "",
+        name: "",
+        status: "",
+        start_date: new Date(),
+        due_date: null,
+        project_paper: {
+            id: null,
+            paper: ""
+        },
+        project: {
+            id: null,
+            name: ""
+        },
+        depends_on: [],
+        next: []
+    });
     const [projectTasks, setProjectTasks] = useState(null);
     
     const [list, setList] = useState("");
     const [name, setName] = useState("");
     const [startDate, setStartDate] = useState(new Date());
-    const [dueDate, setDueDate] = useState(new Date());
+    const [dueDate, setDueDate] = useState(null);
 
-    const addDependsOn = (depId) => {
-        fetch(URL + `api/tasks/${task.id}/depends_on/`, {
+    const addDependsOn = (taskid, depId) => {
+        fetch(URL + `api/tasks/${taskid}/depends_on/`, {
             method: 'POST',
             credentials: "same-origin",
             headers: {
@@ -90,6 +93,52 @@ export default function TaskModal(props) {
         props.handleClose();
     }
 
+    const createTask = () => {
+        fetch(URL + `api/tasks/`, {
+            method: 'POST',
+            credentials: "same-origin",
+            headers: {
+                'Authorization': `Token ${getStorageToken()}`,
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({ 
+                name: name, 
+                status: list, 
+                start_date: startDate,
+                due_date: dueDate,
+                project_id: getStorageProjID(),
+                project_paper_id: task.project_paper.id
+            })
+        })
+        .then(resp=>{
+            if (resp.status >= 400) throw new Error();
+            return resp.json();
+        })
+        .then(resp=>{
+            console.log("here : ", resp)
+            setTask({...task, id:resp.id});
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+        // props.handleClose();
+    }
+
+    const deleteDependsOn = (task_id) => {
+        fetch(URL + `api/tasks/${task.id}/`, {
+            method: 'DELETE',
+            credentials: "same-origin",
+            headers: {
+                'Authorization': `Token ${getStorageToken()}`,
+                'Content-Type':'application/json'
+            },
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
+
     const loadData = async () => {
         // Get the task
         await fetch(URL + `api/tasks/${props.data[0]}`, {
@@ -113,30 +162,51 @@ export default function TaskModal(props) {
             setStartDate(resp.start_date);
             setDueDate(resp.due_date);
 
-            // Get task for dependencies
-            fetch(URL + `api/projects/${task.project.id}`, {
-                method: 'GET',
-                credentials: "same-origin",
-                headers: {
-                    'Authorization': `Token ${getStorageToken()}`,
-                    'Content-Type':'application/json'
-                }
-            })
-            .then(resp=>{
-                if (resp.status >= 400) throw new Error();
-                return resp.json();
-            })
-            .then(resp=>{
-                setProjectTasks(resp.tasks);
-            })
-            .catch(error=>{
-                console.log(error);
-            })
+            // // Get task for dependencies
+            // fetch(URL + `api/projects/${task.project.id}`, {
+            //     method: 'GET',
+            //     credentials: "same-origin",
+            //     headers: {
+            //         'Authorization': `Token ${getStorageToken()}`,
+            //         'Content-Type':'application/json'
+            //     }
+            // })
+            // .then(resp=>{
+            //     if (resp.status >= 400) throw new Error();
+            //     return resp.json();
+            // })
+            // .then(resp=>{
+            //     setProjectTasks(resp.tasks);
+            // })
+            // .catch(error=>{
+            //     console.log(error);
+            // })
         })
         .catch(error=>{
             console.log(error);
         })
     };
+
+    const getTasks = (task_project_id) => {
+        fetch(URL + `api/projects/${task_project_id}`, {
+            method: 'GET',
+            credentials: "same-origin",
+            headers: {
+                'Authorization': `Token ${getStorageToken()}`,
+                'Content-Type':'application/json'
+            }
+        })
+        .then(resp=>{
+            if (resp.status >= 400) throw new Error();
+            return resp.json();
+        })
+        .then(resp=>{
+            setProjectTasks(resp.tasks);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
 
     useEffect(() => {
         console.log(props.data);
@@ -144,10 +214,17 @@ export default function TaskModal(props) {
             loadData();
     }, [props.data]);
     
+
+    useEffect(() => {
+        if(props.projectTask) getTasks(getStorageProjID());
+    }, []);
+
   
     return (task &&
         <Dialog open={props.isOpen} onClose={props.handleClose}>
-            <DialogTitle>Task #{task.id}</DialogTitle>
+            <DialogTitle>{
+                    task.id ? `Task #${task.id}` : "Add Task"
+                }</DialogTitle>
             <DialogContent>
                 <FormControl fullWidth sx={{ mb: 3 }}>
                     <TextField
@@ -201,67 +278,77 @@ export default function TaskModal(props) {
 
                 <hr />
 
-                <FormControl fullWidth sx={{ pb: 3 }}>
-                    <InputLabel id="depends-on">Depends on</InputLabel>
-                    <Select
-                        labelId="depends-on"
-                        id="demo-simple-select"
-                        value={list}
-                        label="List"
-                        onChange={(e) => setList(e.target.value)}
-                    >
-                        {projectTasks && projectTasks.map((data, index) => (
-                            <MenuItem value={data.id} key={index} onClick={() => addDependsOn(data.id)}>
-                                {data.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <nav aria-label="secondary mailbox folders">
-                        <List>
-                            {task && task.depends_on.map((data, index) => (
-                                <ListItem disablePadding>
-                                    <ListItemButton>
-                                    <ListItemText primary={data.before} />
-                                    </ListItemButton>
-                                    <Button>X</Button>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </nav>
-                </FormControl>
 
-                <hr />
-                <FormControl fullWidth sx={{ pb: 3 }}>
-                    <InputLabel id="depends-on">Next</InputLabel>
-                    <Select
-                        labelId="depends-on"
-                        id="demo-simple-select"
-                        value={list}
-                        label="List"
-                        onChange={(e) => setList(e.target.value)}
-                    >
-                        {projectTasks && projectTasks.map((data, index) => (
-                            <MenuItem value={data.id} key={index}>{data.name}</MenuItem>
-                        ))}
-                    </Select>
-                    <nav aria-label="secondary mailbox folders">
-                        <List>
-                            {task && task.next.map((data, index) => (
-                                <ListItem disablePadding>
-                                    <ListItemButton>
-                                    <ListItemText primary={data.after} />
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </nav>
-                </FormControl>
+                {
+                    task.id && 
+                    <React.Fragment>    
+                        <FormControl fullWidth sx={{ pb: 3 }}>
+                            <InputLabel id="depends-on">Depends on</InputLabel>
+                            <Select
+                                labelId="depends-on"
+                                id="demo-simple-select"
+                                // value=""
+                                label="Depends on"
+                                onChange={(e) => setTask({...task, depends_on:task.depends_on.concat(e.target.value)})}
+                            >
+                                {projectTasks && projectTasks.map((data, index) => (
+                                    <MenuItem value={data.id} key={index} onClick={() => addDependsOn(task.id, data.id)}>
+                                        {data.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+
+                            <nav aria-label="secondary mailbox folders">
+                                <List>
+                                    {task.id && task.depends_on.map((data, index) => (
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                            <ListItemText primary={data.before} />
+                                            </ListItemButton>
+                                            <Button onClick={()=>{deleteDependsOn(task.id); setTask({...task, depends_on:task.depends_on.filter(item=> item.before!=data.before)})}}>X</Button>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </nav>
+                        </FormControl>
+
+                        <hr />
+                        <FormControl fullWidth sx={{ pb: 3 }}>
+                            <InputLabel id="depends-on">Next</InputLabel>
+                            <Select
+                                labelId="next"
+                                id="demo-simple-select"
+                                value={task.depends_on ? task.depends_on[0] : ""}
+                                label="Next"
+                                onChange={(e) => setTask({...task, next:task.next.concat(e.target.value)})}
+                            >
+                                {projectTasks && projectTasks.map((data, index) => (
+                                    <MenuItem value={data.id} key={index} onClick={() => addDependsOn(data.id, task.id)}>
+                                        {data.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                            <nav aria-label="secondary mailbox folders">
+                                <List>
+                                    {task && task.next.map((data, index) => (
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                            <ListItemText primary={data.after} />
+                                            </ListItemButton>
+                                            <Button onClick={()=>{deleteDependsOn(task.id); setTask({...task, next:task.next.filter(item=> item.after!=data.after)})}}>X</Button>
+                                        
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </nav>
+                        </FormControl>
+                    </React.Fragment>
+                }
                 
-            </DialogContent>
-
+                </DialogContent>
             <DialogActions>
                 <Button onClick={props.handleClose}>Back</Button>
-                <Button variant="outlined" color="success" onClick={updateTask}>Save</Button>
+                <Button variant="outlined" color="success" onClick={task.id ? updateTask : createTask}>Save</Button>
             </DialogActions>
         </Dialog>
     );
--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,9 +1,47 @@
 import React, { useState, useEffect } from "react";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid'
+import Typography from "@mui/material/Typography"
+import Button, { ButtonProps } from '@mui/material/Button';
+// import SearchBar from "../components/SearchBar"
+import AddIcon from '@mui/icons-material/Add';
+import { grey } from '@mui/material/colors';
+import TaskModal from "../components/TaskModal";
 
 export default function Tasks() {
+    const [search, setSearch] = useState("");
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleModalClose = () => {
+        setOpenModal(false);
+    }
+
+
     return (
         <React.Fragment>
-            <h1>Tasks Page</h1>
+            {/* <h1>Tasks Page</h1> */}
+
+            <Grid container justifyContent="flex-end">
+                {/* <Grid item>
+                    <SearchBar
+                        data={search}
+                        handleSearch={(data) => {setSearch(data); console.log(data)}}
+                    />
+                </Grid> */}
+
+                <Grid item>
+                    <Button 
+                        variant="outlined" 
+                        size="large" 
+                        startIcon={<AddIcon />}
+                        style={{borderColor: "black", color: "black"}}
+                        onClick={()=>{setOpenModal(true)}}
+                        // color="black"
+                    >Create Task</Button>
+                </Grid> 
+            </Grid>
+
+            <TaskModal isOpen={openModal} handleClose={handleModalClose} projectTask={true}/>
         </React.Fragment>
     )
 }

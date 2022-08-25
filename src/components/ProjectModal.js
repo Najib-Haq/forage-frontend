510a9e53f101a413f6988b0e99eef3b99ab4c478
Mushtari Sadia <midnight.scream98@gmail.com>
2022-08-25 13:52:04 +0600

    vertical bar added

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -6,9 +6,11 @@ import { Divider } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
+import { styled } from '@mui/material/styles';
 import FormControl from '@mui/material/FormControl';
-
 import { getStorageToken } from "../context/Auth";
+import MuiGrid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 /* 
     define props=> 
@@ -34,6 +36,15 @@ const style = {
   };
 
 
+const Grid = styled(MuiGrid)(({ theme }) => ({
+    width: '100%',
+    ...theme.typography.body2,
+    '& [role="separator"]': {
+      margin: theme.spacing(0, 2),
+    },
+  }));
+
+
 export default function BasicModal(props) {
     const [data, setData] = useState({
         name: "",
@@ -41,6 +52,46 @@ export default function BasicModal(props) {
         colab: [],
         keywords: []
     });
+
+    const content = (
+        <Box>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+                Create New Project 
+            </Typography>
+
+            <Divider />
+
+            <FormControl fullWidth sx={{ m: 1 }}>
+                <InputLabel htmlFor="outlined-adornment-title">Title</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-title"
+                    value={data.name}
+                    onChange={(event)=>{setData({...data, name:event.target.value})}}
+                    label="Title"
+                />  
+            </FormControl>
+
+            <FormControl fullWidth sx={{ m: 1 }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-description">Description</InputLabel>
+                <OutlinedInput
+                    placeholder="Project Description"
+                    id="outlined-adornment-description"
+                    value={data.description}    
+                    onChange={(event)=>{setData({...data, description:event.target.value})}}
+                    label="Description"
+                    multiline={true}
+                    rows={5}
+                />  
+            </FormControl>
+        </Box>
+      );
+
+      const content2 = (
+        <Box>
+            <Button variant="contained" fullWidth sx={{ m: 1 }}>Create</Button>
+            <Button variant="contained" fullWidth sx={{ m: 1 }}>Cancel</Button>
+        </Box>
+      );
   
     return (
         <Modal
@@ -49,37 +100,18 @@ export default function BasicModal(props) {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-            <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Create New Project 
-                </Typography>
-
-                <Divider />
-
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <InputLabel htmlFor="outlined-adornment-title">Title</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-title"
-                        value={data.name}
-                        onChange={(event)=>{setData({...data, name:event.target.value})}}
-                        label="Title"
-                    />  
-                </FormControl>
-
-                <FormControl fullWidth sx={{ m: 1 }} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-description">Description</InputLabel>
-                    <OutlinedInput
-                        placeholder="Project Description"
-                        id="outlined-adornment-description"
-                        value={data.description}    
-                        onChange={(event)=>{setData({...data, description:event.target.value})}}
-                        label="Description"
-                        multiline={true}
-                        rows={5}
-                    />  
-                </FormControl>
-
-            </Box>
+                    
+                <Grid container sx={style}>
+                    <Grid item xs>
+                        {content}
+                    </Grid>
+                    <Divider orientation="vertical" flexItem style = {{minWidth: "20px"}}>
+                    </Divider>
+                    <Grid item xs style = {{maxWidth: "150px"}}>
+                        {content2}
+                    </Grid>
+                </Grid>
+            
         </Modal>
     );
   }
